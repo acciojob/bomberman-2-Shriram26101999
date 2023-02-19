d335a74ae778f6c0a9d1dad8acd7cb3babdf2ccf
Shriram26101999 <108632713+Shriram26101999@users.noreply.github.com>
2023-02-19 22:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 //your code here
+//your code here
+
+function reset() {
+    for (i = 1; i <= 9; i++) {
+        const block = document.getElementById(`${i}`);
+        block.style.backgroundColor = "transparent";
+    }
+}
+  
+document.getElementById('reset_button').addEventListener('click', reset);
+
+document.getElementById('change_button').addEventListener('click', () => {
+    reset();
+    const blockId = document.getElementById("block_id").value;
+    const color = document.getElementById("colour_id").value;
+    // alert(colorId)
+    const block = document.getElementById(`${blockId}`);
+    block.style.backgroundColor = color;
+});
 
 function reset() {
     for (i = 1; i <= 9; i++) {
